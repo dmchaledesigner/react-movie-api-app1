482a3ec13bccfc1155248372dce380bc40d91405
david mchale <dmchaledesigner@gmail.com>
2021-07-05 14:27:31 +1000

    3. fix search form value

--- src/App.js
+++ src/App.js
@@ -8,16 +8,19 @@ function App() {
 
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState('');
-  const [favourites, setFavourites] = useState([]);
 
 
-  const getMovieData = async () => {
-    const url = "http://www.omdbapi.com/?s=star wars&apikey=283540c6";
+  const getMovieData = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=283540c6`;
 
     const response = await fetch(url);
     const respJson = await response.json()
     const data = respJson.Search;
-    setMovies(data);
+
+    // if there is data
+    if (data) {
+      setMovies(data);
+    }
   }
 
 
@@ -26,8 +29,8 @@ function App() {
 
 
   useEffect(() => {
-    getMovieData();
-  }, [])
+    getMovieData(searchValue);
+  }, [searchValue]);
 
 
 
